14b54f9cd95d397adfcf57225c40d711cf75c82c
Kieu <kieuphan530@gmail.com>
2021-11-11 23:24:58 -0600

    fetch api in js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-// Fetch Animal Crossing villagers API
+document.addEventListener("DOMContentLoaded", () => {
+    // Fetch Animal Crossing villagers API
+    // Part 1 - Fetching villagers
+    fetch("https://acnhapi.com/v1/villagers")
+    .then(resp => resp.json())
+    .then(data => console.log(data))
 
-// Add villagers to page
 
-// Create card or icons to display villagers
+    
+    // Add villagers to page
+    
+    // Create card or icons to display villagers
+    
+    // Event 1 - Click on villagers to show their info
+    
+    // Event 2 - Create a form to submit new villagers
+    
+    // Event 3 - Add villagers to a list when their card is clicked
+    
+    // Stretch Goal - Delete villager from list
 
-// Event 1 - Click on villagers to show their info
+})
 
-// Event 2 - Create a form to submit new villagers
-
-// Event 3 - Add villagers to a list when their card is clicked
-
-// Stretch Goal - Delete villager from list
