9ecd80f9a7af84bda9e2cf730fa2c5e35e0b7e70
Kieu <kieuphan530@gmail.com>
2021-11-11 23:18:48 -0600

    create project plan in js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Fetch Animal Crossing villagers API
+
+// Add villagers to page
+
+// Create card or icons to display villagers
+
+// Event 1 - Click on villagers to show their info
+
+// Event 2 - Create a form to submit new villagers
+
+// Event 3 - Add villagers to a list when their card is clicked
+
+// Stretch Goal - Delete villager from list
