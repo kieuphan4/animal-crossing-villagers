f4671a67b37084dc9e8e4eec6c13b65c9f3a54d6
Kieu <kieuphan530@gmail.com>
2021-11-17 12:09:30 -0600

    create villager detail button

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ const villagerCollection = document.getElementById("villager-container")
 
 document.addEventListener("DOMContentLoaded", () => {
     // Part 1 - Fetch villagers from API
-    
+
     function fetchVillagers() {
         fetch(`http://acnhapi.com/v1a/villagers/`)
-        .then(resp => resp.json())
-        .then(data => data.map(villager => renderVillager(villager)))  
+            .then(resp => resp.json())
+            .then(data => data.map(villager => renderVillager(villager)))
     }
-    
+
     function renderVillager(villager) {
         const villagerCard = document.createElement("div")
         villagerCard.className = "card"
@@ -19,10 +19,13 @@ document.addEventListener("DOMContentLoaded", () => {
         villagerCollection.append(villagerCard)
 
         const detailsBtn = document.createElement("button")
+        detailsBtn.id = `btn${villager.id}`
+        detailsBtn.className = "button"
         detailsBtn.innerHTML = "About Me!"
         villagerCard.append(detailsBtn)
 
         const detailsDiv = document.createElement("div")
+        detailsDiv.id = villager.id
         detailsDiv.className = "details"
         detailsDiv.innerHTML = `
             <p><b>Birthday: </b>${villager["birthday-string"]}</p>
@@ -31,31 +34,30 @@ document.addEventListener("DOMContentLoaded", () => {
             <p><b>Personality: </b>${villager.personality}</p>
             <p><b>Hobby: </b>${villager.hobby}</p>
             <p><b>Catchphrase: </b>${villager["catch-phrase"]}</p>
-            <p><b>Saying: </b>${villager.saying}</p> `
+            <p><b>Saying: </b>${villager.saying}</p>`
         villagerCard.append(detailsDiv)
-        
-        // detailsBtn.addEventListener("click", () => {
-        //     document.querySelectorAll(".details").forEach(function(btn) {
-        //         if (btn.style.display === "none") {
-        //             btn.style.display = "block";
-        //         } else {
-        //             btn.style.display = "none";
-        //         }
-                
-        //     })
-        // })
+
+        detailsBtn.addEventListener("click", (e) => {
+            const villagerId = e.target.id.substring(3)
+            let villagerDiv = document.getElementById(villagerId)
+            if (villagerDiv.style.display === "none") {
+                villagerDiv.style.display = "block";
+            } else {
+                villagerDiv.style.display = "none";
+            }
+        })
     }
-    
-    
+
+
     fetchVillagers();
     // Create card or icons to display villagers
-    
+
     // Event 1 - Click on villagers to show their info
-    
+
     // Event 2 - Create a form to submit new villagers
-    
+
     // Event 3 - Add villagers to a list when their card is clicked
-    
+
     // Stretch Goal - Delete villager from list
 
 })
