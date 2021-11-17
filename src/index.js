63bf76db45146d0a518cfe2b8e24ef25f3a63c2d
Kieu <kieuphan530@gmail.com>
2021-11-16 19:46:56 -0600

    rendering villagers updated

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
+const mainContainer = document.getElementsByClassName("container");
 const villagerCollection = document.getElementById("villager-container")
 
 document.addEventListener("DOMContentLoaded", () => {
     // Part 1 - Fetch villagers from API
-
+    
     function fetchVillagers() {
-        fetch(`http://acnhapi.com/v1a/villagers/`, {
-        })
-            .then(resp => resp.json())
-            .then(data => data.map(villager => renderVillager(villager)))  
+        fetch(`http://acnhapi.com/v1a/villagers/`)
+        .then(resp => resp.json())
+        .then(data => data.map(villager => renderVillager(villager)))  
     }
-
+    
     function renderVillager(villager) {
-        const villagerCard = `<div id="card">
+        const villagerCard = document.createElement("div")
+        villagerCard.className = "card"
+        // const img = villager.image_uri
+
+        villagerCard.innerHTML = `
         <img src="${villager.image_uri}" class="villager-avatar"/>
         <h4>${villager.name["name-USen"]}</h4>
-        </div>`
+        `
+        villagerCollection.append(villagerCard)
 
-        villagerCollection.innerHTML += villagerCard;
+    
+        
+        // `<div class="card">
+        //     <p>Birthday: ${villager["birthday-string"]}</p>
+        //     <p>Gender: ${villager.gender}</p>
+        //     <p>Species: ${villager.species}</p>
+        //     <p>Personality: ${villager.personality}</p>
+        //     <p>Hobby: ${villager.hobby}</p>
+        //     <p>Catchphrase: ${villager["catch-phrase"]}</p>
+        //     <p>Saying: ${villager.saying}</p>
+        // </div>`
+    
+        // villagerCollection.innerHTML += villagerCard;
     }
     
     
