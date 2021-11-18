7bc1d1912c7f00461228e6fc6fdd003f8754ce84
Kieu <kieuphan530@gmail.com>
2021-11-17 23:35:50 -0600

    page styling update to fit screen

--- src/index.js
+++ src/index.js
@@ -10,12 +10,14 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(data => data.map(villager => renderVillager(villager)))
     }
 
+    // Part 2 - Create card or icons to display villagers
     function renderVillager(villager) {
         const villagerCard = document.createElement("div")
         villagerCard.className = "card"
         villagerCard.innerHTML = `
             <img src="${villager.image_uri}" class="villager-avatar"/>
-            <h4>${villager.name["name-USen"]}</h4>`
+            <p>"${villager.saying}"</p>
+            <h4><b>${villager.name["name-USen"]}</b></h4>`
         villagerCollection.append(villagerCard)
 
         const detailsBtn = document.createElement("button")
@@ -33,17 +35,17 @@ document.addEventListener("DOMContentLoaded", () => {
             <p><b>Species: </b>${villager.species}</p>
             <p><b>Personality: </b>${villager.personality}</p>
             <p><b>Hobby: </b>${villager.hobby}</p>
-            <p><b>Catchphrase: </b>${villager["catch-phrase"]}</p>
-            <p><b>Saying: </b>${villager.saying}</p>`
-        villagerCard.append(detailsDiv)
-
+            <p><b>Catchphrase: </b>${villager["catch-phrase"]}</p>`
+            villagerCard.append(detailsDiv)
+            
+        // Event 1 - Click on villagers to show their info
         detailsBtn.addEventListener("click", (e) => {
             const villagerId = e.target.id.substring(3)
             let villagerDiv = document.getElementById(villagerId)
-            if (villagerDiv.style.display === "none") {
-                villagerDiv.style.display = "block";
-            } else {
+            if (villagerDiv.style.display === "block") {
                 villagerDiv.style.display = "none";
+            } else {
+                villagerDiv.style.display = "block";
             }
             e.target.style.background = "#66bb6a"
         })
@@ -51,13 +53,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
     fetchVillagers();
-    // Create card or icons to display villagers
 
-    // Event 1 - Click on villagers to show their info
 
-    // Event 2 - Create a form to submit new villagers
+    // Event 2 - Create a search for villager
 
-    // Event 3 - Add villagers to a list when their card is clicked
+    // Event 3 - Add villagers to a favorite list when their card is clicked
 
     // Stretch Goal - Delete villager from list
 
